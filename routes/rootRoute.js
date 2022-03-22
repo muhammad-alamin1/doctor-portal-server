9c8cd617ba54f213ce5bac6ccc92e22129ea29a7
Muhammad <muhammad@kali.kali>
2022-03-22 11:05:23 +0600

    final commit

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -1,6 +1,6 @@
 const rootRouter = require('express').Router({ caseSensitive: true });
 
-rootRouter.post('/', (req, res) => {
+rootRouter.get('/', (req, res) => {
     res.status(200).json({
         success: true,
         message: 'Root API Server'
